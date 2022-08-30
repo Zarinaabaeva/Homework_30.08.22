e953f679e3dea41ab394d758a1e243a25d1c0b2a
Zarina <abaevazarina@yandex.ru>
2022-08-31 01:17:40 +0300

    task 2 solved

---
+++ homework_30.08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b){
+    int max = a;
+    Console.WriteLine(a);
+}
+else{
+    Console.WriteLine(b);
+}
