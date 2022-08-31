34ed44d6532a1c09fd3a28a0b6d4364e09be556c
Zarina <abaevazarina@yandex.ru>
2022-08-31 03:58:26 +0300

    Решить задачу в task4

---
+++ task4/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+while(a > max){
+
+if(a > max){
+    Console.WriteLine(a);
+}
+else{
+    Console.WriteLine(max);
+}
+
+if(b > max){
+    Console.WriteLine(b);
+}
+else{
+    Console.WriteLine(max);
+}
+
+if(c > max){
+    Console.WriteLine(c);
+   
+}
+else{
+    Console.WriteLine(max);
+}
+}
+
+
+
