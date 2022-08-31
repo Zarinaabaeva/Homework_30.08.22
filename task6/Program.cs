3ab2f5e04d0c681ab80b025a011331e761bd9fa3
Zarina <abaevazarina@yandex.ru>
2022-08-31 12:01:44 +0300

    task6 solved

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if(a % 2 == 0 ){
+    Console.WriteLine("Да");
+}
+
+else{
+    Console.WriteLine("Нет");
+}
